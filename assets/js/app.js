85a7fdf82b13d1d812f85609dc4b6285933a0a70
kplam624 <kplam624@gmail.com>
2021-01-10 17:20:36 -0500

    created the svg.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+// Creating the svg
+
+var svgWidth = 900;
+var svgHeight = 500;
+
+var margin ={
+    top : 20,
+    bottom: 40,
+    left: 60,
+    right: 80
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.bottom - margin.top;
+
+
