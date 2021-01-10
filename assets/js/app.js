e13f30c5b2608ed8750b7c9b33e18e50b1ff191a
kplam624 <kplam624@gmail.com>
2021-01-10 18:15:53 -0500

    Adds the svg wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,4 +13,48 @@ var margin ={
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.bottom - margin.top;
 
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("./data/data.csv").then(function(healthData){
+    
+    healthData.forEach(function(data){
+        data.age = +data.age;
+        data.smokes = +data.smokes;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([d3.min(healthData, d => d.age) * 0.8 ,d3.max(healthData, d => d.age) * 1.2])
+        .range([0,width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([d3.min(healthData, d => d.smokes) * 0.8, d3.max(healthData, d => d.smokes) * 1.2])
+        .range([height,0]);
+    
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    var xAxis = chartGroup.append("g")
+        .classed("x-axis", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    chartGroup.selectAll("circle")
+        .data(hairData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.age))
+        .attr("cy", d => yLinearScale(d.smokes))
+        .attr("r", 20)
+        .attr("fill", "pink")
+        .attr("opacity", ".5");
+
+});
